78d43079e116d5263c76c4bbea25d5a8060b5ccf
jessekerch <jesse.kercheval@gmail.com>
2024-09-23 12:40:29 -0700

    day 1, react components

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "./components/App";
+import ReactDOM from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(<App />);
+
+const ce = React.createElement;
